518c8c9b9232568556ee23879262bbc64f9c584d
Alison Kanegae <alisonkanegae@gmail.com>
2018-08-27 04:56:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var recipes = new Object();
 
 //2
 function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({}, {[key]: value})
+  return Object.assign({}, object, {[key]: value})
 }
 
 //3
