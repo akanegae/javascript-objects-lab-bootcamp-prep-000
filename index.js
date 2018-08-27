e5def38223c17c0816b235f9ebd7dffcf3e580c3
Alison Kanegae <alisonkanegae@gmail.com>
2018-08-27 04:53:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
   return object;
 }
+//4
+function deleteFromObjectByKey(object, key) {
+  var newObj = Object.assign({}, object);
+  return newObj;
+}
